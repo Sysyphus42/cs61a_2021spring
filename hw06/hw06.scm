e6bbd05a133b4f420f769f1b7c5182d0b38e7654
liuh42 <59326351+liuh42@users.noreply.github.com>
2023-12-01 11:43:41 +0800

    Add files via upload

---
+++ hw06/hw06.scm
@@ -0,0 +1,23 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cddr s)))
+
+(define (sign val) 
+    (cond
+        ((> val 0) 1) 
+        ((= val 0) 0) 
+        ((< val 0) -1)  
+    )  
+
+)
+
+(define (square x) (* x x))
+
+(define (pow base exp) 
+    (cond
+        ((= base 1) 1)
+        ((= exp 0) 1)
+        ((even? exp) (square (pow base (/ exp 2))))
+        (else (* base (square (pow base (/ (- exp 1) 2)))))))
